bc0ac417b094b4ed7f4570b8494f3726298bfcb9
Pierp23 <ryu.no.seishin.21@gmail.com>
2023-07-14 17:25:10 +0200

    Project initiation

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -14,8 +14,47 @@ export default {
 
 <template>
     <header>
-        HEADER
+        <div class="header-logo">
+            <img src="../assets/img/dc-logo.png" alt="logo-DC">
+        </div>
+        <div class="nav">
+            <li>
+                Characters
+            </li>
+            <li>
+                Comics
+            </li>
+            <li>
+                Movies
+            </li>
+            <li>
+                Tv
+            </li>
+            <li>
+                Games
+            </li>
+            <li>
+                Collectibles
+            </li>
+            <li>
+                Videos
+            </li>
+            <li>
+                Fans
+            </li>
+            <li>
+                News
+            </li>
+            <li>
+                Shop
+            </li>
+
+        </div>
     </header>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+header .nav {
+    text-transform: uppercase;
+}
+</style>
